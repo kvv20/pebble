780de4fc4b17004fc699c0f7e67fa2f40007b90d
kvv20 <kvv20@users.noreply.github.com>
2015-05-08 15:40:37 -0400

    stylish font for bottom line

--- src/main.c
+++ src/main.c
@@ -21,8 +21,8 @@ static void update_sky(){
   BatteryChargeState charge_state = battery_state_service_peek();
   snprintf(battery_buffer, sizeof(battery_buffer), "%d%%", charge_state.charge_percent);
   // Create a long-lived buffer
-  static char date_buffer[] = "Thu, Apr 01     100% ";
-  strftime(date_buffer, sizeof("Thu, Apr 01     100%"), "%a, %b %d      ", tick_time);
+  static char date_buffer[] = "Thu, Apr 01         100% ";
+  strftime(date_buffer, sizeof("Thu, Apr 01         100%"), "%a, %b %d         ", tick_time);
   strcat(date_buffer, battery_buffer);
   text_layer_set_text(s_date_layer, date_buffer);
   GBitmap *curBitmap;
@@ -115,7 +115,8 @@ static void main_window_load(Window *window) {
   text_layer_set_text_color(s_date_layer, GColorBlack);
 
   // Apply to TextLayer for date
-  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  //text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
 
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
